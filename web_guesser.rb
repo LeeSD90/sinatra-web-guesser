f68bd34e94e01d76f128473defa2b2a21a11446d
LeeSD90 <lee.doyle90@gmail.com>
2018-04-11 15:01:30 +0200

    Added feedback based on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(101)
+SECRET_NUMBER = rand(101)
 
 get '/' do
-	erb :index, :locals => {:number => number}
+	guess = params["guess"].to_i
+	number = "SECRET"
+
+	case
+	when (guess - SECRET_NUMBER) > 5
+		text = "Way too high!"
+		
+	when (guess - SECRET_NUMBER) < -5
+		text = "Way too low!"
+		
+	when guess > SECRET_NUMBER
+		text = "Too high!"
+		
+	when guess < SECRET_NUMBER
+		text = "Too low!"
+		
+	when guess == SECRET_NUMBER
+		text = "Correct!"
+		number = SECRET_NUMBER.to_s
+	end
+
+	erb :index, :locals => {:number => number, :text => text}
+	#throw params. inspect
 end
